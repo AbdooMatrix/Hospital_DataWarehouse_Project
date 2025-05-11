7362266186492baaf6bacb9f7c258c36a5772dba
Abdelrahman Mostafa <abdomostafa20188@gmail.com>
2025-05-11 20:46:07 +0300

    edit

--- Hospital_Management_System_Database/QueriesKPIs.sql
+++ Hospital_Management_System_Database/QueriesKPIs.sql
@@ -72,9 +72,8 @@ GROUP BY
     fa.Doctor_Surrogate_Id,
     CONCAT(dd.first_name, ' ', dd.last_name);
 
--- 3. Top 5 Doctors by Appointment Volume
-SELECT TOP 5
-	dd.Year,
+-- 3. Top Doctors by Appointment Volume
+SELECT
     CONCAT(d.First_Name, ' ', d.Last_Name) AS Doctor_Name,
     COUNT(fa.Appointment_Id) AS Total_Appointments
 FROM 
@@ -82,7 +81,6 @@ FROM
     INNER JOIN Dim_Doctor d ON fa.Doctor_Surrogate_Id = d.Doctor_Surrogate_Id
 	INNER JOIN Dim_Date dd ON fa.Appointment_Date_Key = dd.Date_Key
 GROUP BY 
-	dd.Year,
     d.Doctor_Surrogate_Id,
     d.First_Name,
     d.Last_Name
@@ -136,8 +134,8 @@ GROUP BY
 ORDER BY 
     dd.Year, dd.Month;
 
--- 4. Top 5 Patients by Total Billing in each year
-SELECT TOP 5
+-- 4. Top Patients by Total Billing
+SELECT
 	dp.Patient_Surrogate_Id,
     CONCAT(dp.First_Name, ' ', dp.Last_Name) AS Patient_Full_Name,
     SUM(fb.Total_Amount) AS Total_Spent
@@ -153,13 +151,12 @@ ORDER BY
 --===========================================================================================--
 
 -- Fact Cleaning Service
--- 1. Measures the Total number of cleaning services performed by each staff member per room over each year.
+-- 1. Total No. of cleaning services done by each staff member in each year
 SELECT 
     dd.Year,
     fcs.Staff_Surrogate_Id,
     CONCAT(ds.First_Name, ' ', ds.Last_Name) AS Staff_Name,
-	dr.Room_Surrogate_Id,
-    SUM(fcs.Service_Count) AS Total_Cleaning_Services_Per_Room
+    SUM(fcs.Service_Count) AS Total_Cleaning_Services
 FROM 
     Fact_Cleaning_Service fcs
     INNER JOIN Dim_Date dd ON fcs.Service_Date_Key = dd.Date_Key
@@ -168,16 +165,14 @@ FROM
 GROUP BY 
     dd.Year,
     fcs.Staff_Surrogate_Id,
-    CONCAT(ds.First_Name, ' ', ds.Last_Name),
-	dr.Room_Surrogate_Id
+    CONCAT(ds.First_Name, ' ', ds.Last_Name)
 ORDER BY 
     dd.Year,
     fcs.Staff_Surrogate_Id;
 
--- 2. Monthly Cleaning Services per Room
+-- 2. Yearly Cleaning Services per Room
 SELECT 
     dd.Year,
-    dd.Month,
     dr.Room_Number,
     SUM(fcs.Service_Count) AS Total_Services
 FROM 
@@ -186,39 +181,12 @@ FROM
     INNER JOIN Dim_Date dd ON fcs.Service_Date_Key = dd.Date_Key
 GROUP BY 
     dd.Year,
-    dd.Month,
     dr.Room_Surrogate_Id,
     dr.Room_Number
 ORDER BY 
-    dd.Year ASC, dd.Month ASC, Total_Services DESC;
+    dd.Year ASC, Total_Services DESC;
 
--- 3. Average Daily Cleaning Services per Staff Member in DESC order
-SELECT 
-	Daily.Staff_Surrogate_Id,
-    Daily.Staff_Name,
-    AVG(Daily.Services_Per_Day) AS Avg_Daily_Services
-FROM (
-    SELECT 
-        fcs.Staff_Surrogate_Id,
-        dd.Full_Date,
-        CONCAT(ds.First_Name, ' ', ds.Last_Name) AS Staff_Name,
-        SUM(fcs.Service_Count) AS Services_Per_Day
-    FROM 
-        Fact_Cleaning_Service fcs
-        INNER JOIN Dim_Staff ds ON fcs.Staff_Surrogate_Id = ds.Staff_Surrogate_Id
-        INNER JOIN Dim_Date dd ON fcs.Service_Date_Key = dd.Date_Key
-    GROUP BY 
-        fcs.Staff_Surrogate_Id,
-        dd.Full_Date,
-        ds.First_Name,
-        ds.Last_Name
-) AS Daily
-GROUP BY 
-    Daily.Staff_Surrogate_Id, Daily.Staff_Name
-ORDER BY 
-    Avg_Daily_Services DESC;
-
--- 4. Total Number of Cleaning Services by Room Type
+-- 3. Total Number of Cleaning Services by Room Type
 SELECT 
     dr.Room_Type,
     SUM(fcs.Service_Count) AS Total_Services
